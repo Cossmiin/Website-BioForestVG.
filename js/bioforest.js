fbb23bb452ae905e662eb74e1c0fd0bf6ee3fdcc
Izot Cosmin <izotcosmin@MacBook-Pro-Izot.local>
2023-11-06 20:43:44 +0200

    actualizare

--- js/bioforest.js
+++ js/bioforest.js
@@ -1,19 +1,19 @@
 window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    const scrolled = window.scrollY > 0;
-  
-    if (scrolled) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
+  const navbar = document.getElementById('navbar');
+  const scrolled = window.scrollY > 0;
+
+  if (scrolled) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenu = document.querySelector(".burger");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  burgerMenu.addEventListener("click", function () {
+      mobileMenu.classList.toggle("show");
   });
-
-  document.addEventListener("DOMContentLoaded", function () {
-    const burgerMenu = document.querySelector(".burger");
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    burgerMenu.addEventListener("click", function () {
-        mobileMenu.classList.toggle("show");
-    });
 });
